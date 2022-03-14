15c5a64d065099678804421f41436e523cf10404
Alexander <alexander@angryrobot.ru>
2022-03-14 12:01:09 +0300

    Added github link

--- app/src/main/java/ru/angryrobot/barriertape/demo/fragments/AboutFragment.kt
+++ app/src/main/java/ru/angryrobot/barriertape/demo/fragments/AboutFragment.kt
@@ -1,5 +1,8 @@
 package ru.angryrobot.barriertape.demo.fragments
 
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +11,20 @@ import ru.angryrobot.barriertape.demo.databinding.AboutFragmentBinding
 
 class AboutFragment : BaseFragment() {
 
+    val url = "https://github.com/alexmedv/BarrierTape"
+
     private lateinit var binding: AboutFragmentBinding
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View {
         binding = AboutFragmentBinding.inflate(inflater, container, false)
+        binding.githubLink.apply {
+            text = url
+            paintFlags = paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        }
+
+        binding.githubLinkBlock.setOnClickListener {
+            requireActivity().startActivity(Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(url) })
+        }
         return binding.root
     }
 }
